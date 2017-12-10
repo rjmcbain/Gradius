45b5353413d2ddb0325155315aacc4c025fd9078
rjmcbain <rmcbain01@gmail.com>
2017-12-09 17:43:53 -0700

    added player 2 and start button

--- js/app.js
+++ js/app.js
@@ -44,10 +44,52 @@ function movePlayer () {
      
 };
 
+var keys = [];
+
+$("html").keydown(function(event){
+
+    if (keys.length > 2) return;
+    keys.push (event.which);
+    
+    movePlayer2();
+});
+
+function movePlayer2 () {
+    var moveTo = keys[0];
+    if(moveTo == "65") {
+      $("#player2").stop(true).animate({"left" : "-=30px"});
+    }
+
+    if(moveTo == "68") {
+      $("#player2").stop(true).animate({"left" : "+=30px"});
+    }
+
+    if(moveTo == "87") {
+      $("#player2").stop(true).animate({"top" : "-=30px"});
+    }
+
+    if(moveTo == "83") {
+      $("#player2").stop(true).animate({"top" : "+=30px"});
+    }
+    
+     keys = keys.slice (1);
+
+     
+};
+
+
 	$(function(){
 		$('#hide').click(function(){
 			$("button").hide();
+			$("#instructions").hide();
+			$(".blue").hide();
 		});
 	});
 
+
+
+$('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
  
