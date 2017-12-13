f4f4e5f685dae6ed4ec6d88f03bdb505bc5a2e23
rjmcbain <rmcbain01@gmail.com>
2017-12-12 20:37:22 -0700

    added timer

--- js/app.js
+++ js/app.js
@@ -1,163 +1,4 @@
 console.log("hello");
-
-// $(function(){
-//  var gamepiece = "player";
-//  $('#gameboard').append(player);
-
-//  $(document).keydown(function(e){
-
-    
-//  })
-
-// });
-
-setTimeout (function(){
-  $('#fade').fadeOut('slow');                      //intro text
-}, 6000);   
-
-$(function(){
-    $('.fadeIn').hide();
-    setTimeout(function(){                           //intro text
-    $('.fadeIn').fadeIn('slow');    
-    }, 17750);
-})
-
-$(function(){
-    $('.hard').hide();
-    setTimeout(function(){                           //intro text
-    $('.hard').fadeIn('slow');    
-    }, 28750);
-})
-
-$(function(){
-    $('.delayOut').show();
-    setTimeout(function(){                           //intro text
-    $('.delayOut').fadeOut('slow');    
-    }, 27750);
-})
-
-$(function(){
-    $('.p1').hide();
-    setTimeout(function(){                           //intro text
-    $('.p1').fadeIn('slow');  
-    }, 17750);
-})
-
-$(function(){
-    $('.p2').hide();
-    setTimeout(function(){                           //intro text
-    $('.p2').fadeIn('slow');  
-    }, 17750);
-})
-
-
-// $(document).ready(function() {
-//     $("#boss").animate({top: "+=200"}, 5500);           //boss movement
-//     $("#boss").animate({top: "-=400"}, 500);
-//     $("#boss").animate({top: "+=100"}, 500);
-//     $("#boss").animate({left: "-=450"}, 500);
-//     $("#boss").animate({left: "+=450"}, 2000);
-//     $("#boss").animate({left: "-=100"}, 750);
-//     $("#boss").animate({top: "+=100"}, 750);
-//     $("#boss").animate({top: "-=100"}, 750);
-//     $("#boss").animate({top: "+=250"}, 500);
-//     $("#boss").animate({top: "-=300"}, 500);           //boss movement
-//     $("#boss").animate({top: "+=150"}, 500);
-//     $("#boss").animate({left: "+=100"}, 500);
-//     $("#boss").animate({top: "+=200"}, 750);
-//     $("#boss").animate({top: "-=400"}, 400);
-//     $("#boss").animate({top: "+=100"}, 700);
-//     $("#boss").animate({left: "-=200"}, 700);
-//     $("#boss").animate({left: "+=200"}, 800);
-//     $("#boss").animate({left: "-=100"}, 2000);          //boss movement
-//     $("#boss").animate({top: "+=100"}, 2000);
-//     $("#boss").animate({top: "-=100"}, 1000);
-//     $("#boss").animate({top: "+=250"}, 1500);
-//     $("#boss").animate({top: "-=300"}, 2500);
-//     $("#boss").animate({top: "+=150"}, 1500);
-//     $("#boss").animate({left: "+=100"}, 1500);
-//     $("#boss").animate({top: "+=200"}, 2000);
-//     $("#boss").animate({top: "-=400"}, 1000);
-//     $("#boss").animate({top: "+=100"}, 2000);
-//     $("#boss").animate({left: "-=200"}, 2000);          //boss movement
-//     $("#boss").animate({left: "+=200"}, 1500);
-//     $("#boss").animate({left: "-=100"}, 3000);
-//     $("#boss").animate({top: "+=100"}, 2000);
-//     $("#boss").animate({top: "-=100"}, 1000);
-//     $("#boss").animate({top: "+=250"}, 1500);
-//     $("#boss").animate({top: "-=300"}, 2500);
-//     $("#boss").animate({top: "+=150"}, 1500);
-//     $("#boss").animate({left: "+=100"}, 1500);
-//     $("#boss").animate({top: "+=200"}, 2000);
-//     $("#boss").animate({top: "-=400"}, 1000);
-//     $("#boss").animate({top: "+=100"}, 2000);
-//     $("#boss").animate({left: "-=200"}, 2000);
-//     $("#boss").animate({left: "+=200"}, 1500);
-//     $("#boss").animate({left: "-=100"}, 3000);          //boss movement
-//     $("#boss").animate({top: "+=100"}, 2000);
-//     $("#boss").animate({top: "-=100"}, 1000);
-//     $("#boss").animate({top: "+=250"}, 1500);
-//     $("#boss").animate({top: "-=300"}, 2500);
-//     $("#boss").animate({top: "+=150"}, 1500);
-//     $("#boss").animate({left: "+=100"}, 1500);
-// });
-
-// $(document).ready(function(){
-//     animateDiv();
-//     animateDiv2();
-    
-// });
-
-// function makeNewPosition(){
-    
-//     // Get viewport dimensions (remove the dimension of the div)
-//     var h = $(window).height() - 50;
-//     var w = $(window).width() - 1;
-    
-//     var nh = Math.floor(Math.random() * h);
-//     var nw = Math.floor(Math.random() * w);
-    
-//     return [nh,nw];    
-    
-// }
-
-// function animateDiv(){
-//     var newq = makeNewPosition();
-//     var oldq = $('#boss').offset();
-//     var speed = calcSpeed([oldq.top, oldq.left], newq);
-    
-//     $('#boss').animate({ top: newq[0], left: newq[1] }, speed, function(){
-//       animateDiv();        
-//     });
-    
-// };
-
-// function animateDiv2(){
-//     var newq = makeNewPosition();
-//     var oldq = $('#boss2').offset();
-//     var speed = calcSpeed([oldq.top, oldq.left], newq);
-    
-//     $('#boss2').animate({ top: newq[0], left: newq[1] }, speed, function(){
-//       animateDiv();        
-//     });
-    
-// };
-
-// function calcSpeed(prev, next) {
-    
-//     var x = Math.abs(prev[1] - next[1]);
-//     var y = Math.abs(prev[0] - next[0]);
-    
-//     var greatest = x > y ? x : y;
-    
-//     var speedModifier = 0.5;
-
-//     var speed = Math.ceil(greatest/speedModifier);
-
-//     return speed;
-
-// }
-
 $(document).ready(function(){
     animateDiv('#boss');
     animateDiv('#boss2');
@@ -168,14 +9,36 @@ $(document).ready(function(){
 });
 
 $("#boss").animate({top: "-=0"}, 7000);
-// $("#boss3").animate({top: "-=0"}, 7000);
-// $("#boss4").animate({top: "-=0"}, 7000);
-// $("#boss5").animate({top: "-=0"}, 7000);
-// $("#boss6").animate({top: "-=0"}, 7000);
+$("#boss2").animate({top: "-=0"}, 7000);
+$("#boss3").animate({top: "-=0"}, 7000);
+$("#boss4").animate({top: "-=0"}, 7000);
+
+$(document).ready(function() {
+    $("#player1").animate({top: "-=0"}, 2000);
+    $("#player1").animate({left: "+=300"}, 4000);
+    $("#player2").animate({top: "-=0"}, 2000);
+    $("#player2").animate({left: "+=400"}, 4000);
+});
+
+setTimeout (function(){
+  $('#fade').fadeOut('slow');                      //intro text
+}, 6000); 
+
+$(function(){
+    $('.countdownfade').hide();
+    setTimeout(function(){                           //intro text
+    $('.countdownfade').fadeIn('slow');    
+    }, 31000);
+    $('.countdown').hide();
+    setTimeout(function(){                           //intro text
+    $('.countdown').fadeIn('slow');    
+    }, 6000);
+})
+
+
+
 function makeNewPosition(){
     
-   
-    // Get viewport dimensions (remove the dimension of the div)
     var h = $(window).height() - 1;
     var w = $(window).width() - 1;
     
@@ -190,7 +53,6 @@ function animateDiv(mydiv){
     var newq = makeNewPosition();
     $(mydiv).animate({ top: newq[0], left: newq[1] }, 500,   function(){
       animateDiv(mydiv);   
-    // setTimeout(animateDiv, 5000);
     });
     
 };
@@ -203,62 +65,22 @@ function animateDiv(mydiv){
       }
     });
   };
-
   // Schedule the update to happen once every second
   setInterval(doUpdate, 1000);
 
-  var delay = 30000; 
+  var doUpdateAgain = function() {
+    $('.countdownfade').each(function() {
+      var count = parseInt($(this).html());
+      if (count !== 0) {
+        $(this).html(count - 1);
+      }
+    });
+  };
+  // Schedule the update to happen once every second
+  setInterval(doUpdateAgain, 1000);
+
+  var delay = 37000; 
 setTimeout(function(){ window.location = 'credits.html'; }, delay);
-
-// $(document).ready(function() {
-//     $("#bossintro").animate({top: "-=0"}, 1800);        //ship movement
-//     $("#bossintro").animate({top: "-=500"}, 8250);
-//     $("#bossintro").animate({left: "-=2500"}, 1000);
-// });
-
-// $(document).ready(function() {
-//     $("#p1intro").animate({top: "-=0"}, 10500);
-//     $("#p1intro").animate({left: "+=6000"}, 5000);      //ship movement
-// });
-
-// $(document).ready(function() {
-//     $("#p2intro").animate({top: "-=0"}, 12000);
-//     $("#p2intro").animate({left: "-=6000"}, 5000);      //ship movement
-// });
-
-// $(document).ready(function() {
-//     $("#p3intro").animate({top: "-=0"}, 13750);         
-//     $("#p3intro").animate({left: "+=6000"}, 5000);      //ship movement
-// });
-
-// $(document).ready(function() {
-//     $("#p4intro").animate({top: "-=0"}, 15000);
-//     $("#p4intro").animate({left: "-=6000"}, 5000);
-// });
-
-// $(document).ready(function() {
-//     $("#player1").animate({top: "-=0"}, 2000);
-//     $("#player1").animate({left: "+=500"}, 4000);
-// });
-
-$(document).ready(function() {
-    $("#bossintro").animate({top: "-=0"}, 1800);        //ship movement
-    $("#bossintro").animate({top: "-=500"}, 8250);
-    $("#bossintro").animate({left: "-=2500"}, 1000);
-    $("#p1intro").animate({top: "-=0"}, 10500);
-    $("#p1intro").animate({left: "+=6000"}, 5000);
-    $("#p2intro").animate({top: "-=0"}, 12000);
-    $("#p2intro").animate({left: "-=6000"}, 5000); 
-    $("#p3intro").animate({top: "-=0"}, 13750);         
-    $("#p3intro").animate({left: "+=6000"}, 5000);
-    $("#p4intro").animate({top: "-=0"}, 15000);
-    $("#p4intro").animate({left: "-=6000"}, 5000);
-    $("#player1").animate({top: "-=0"}, 2000);
-    $("#player1").animate({left: "+=500"}, 4000);
-    $("#player2").animate({top: "-=0"}, 2000);
-    $("#player2").animate({left: "+=600"}, 4000);
-});
-
 
 function getPositions(box) {
   var $box = $(box);
@@ -287,39 +109,6 @@ function checkCollisions(){
     }
 }
 
-
-// var keys = [];
-
-// $("html").keydown(function(event){
-
-//     if (keys.length > 2) return;
-//     keys.push (event.which);
-    
-//     movePlayer();
-// });
-
-// function movePlayer () {
-//     var moveTo = keys[0];
-//     if(moveTo == "37") {
-//       $("#player1").go(true).animate({"left" : "-=30px"});     //player 1
-//     }                                //movement
-
-//     if(moveTo == "39") {
-//       $("#player1").go(true).animate({"left" : "+=30px"});
-//     }
-
-//     if(moveTo == "38") {
-//       $("#player1").go(true).animate({"top" : "-=30px"});
-//     }
-
-//     if(moveTo == "40") {
-//             $("#player1").go(true).animate({"top" : "+=30px"});
-//     }
-    
-//      keys = keys.slice (1);
-
-     
-// };
 
 $(document).keydown(function(e) {
     switch (e.which) {
@@ -392,50 +181,6 @@ $(document).keydown(function(e) {
     }
 
 })
-
-// var keys = [];
-
-// $("html").keydown(function(event){
-
-//     if (keys.length > 2) return;
-//     keys.push (event.which);
-    
-//     movePlayer2();
-// });
-
-// function movePlayer2 () {
-//     var moveTo = keys[0];
-//     if(moveTo == "65") {
-//       $("#player2").stop(true).animate({"left" : "-=30px"});
-//     }
-
-//     if(moveTo == "68") {
-//       $("#player2").stop(true).animate({"left" : "+=30px"});   //player 2
-//     }                                //movement
-
-//     if(moveTo == "87") {
-//       $("#player2").stop(true).animate({"top" : "-=30px"});
-//     }
-
-//     if(moveTo == "83") {
-//       $("#player2").stop(true).animate({"top" : "+=30px"});
-//     }
-    
-//      keys = keys.slice (1);
-
-     
-// };
-
-
-  // $(function(){
-  //  $('#hide').click(function(){
-  //    $("button").slideToggle(200);
-  //    $("#instructions").slideToggle(1500);     Old Hide Button
-  //    $(".blue").slideToggle(2500);       Replaced with Fade
-  //  });
-  // });
-
-
 
 $('html, body').css({
     overflow: 'hidden',       //disables scrolling
--- js/credits.js
+++ js/credits.js
@@ -26,6 +26,67 @@ $(function(){
 $('html, body').css({
     overflow: 'hidden',       //disables scrolling
     height: '100%'
+});  
+
+$(function(){
+    $('.fadeIn').hide();
+    setTimeout(function(){                           //intro text
+    $('.fadeIn').fadeIn('slow');    
+    }, 17750);
+})
+
+$(function(){
+    $('.hard').hide();
+    setTimeout(function(){                           //intro text
+    $('.hard').fadeIn('slow');    
+    }, 31750);
+})
+
+$(function(){
+    $('.delayOut').show();
+    setTimeout(function(){                           //intro text
+    $('.delayOut').fadeOut('slow');    
+    }, 27750);
+})
+
+$(function(){
+    $('.p1').hide();
+    setTimeout(function(){                           //intro text
+    $('.p1').fadeIn('slow');  
+    }, 17750);
+})
+
+$(function(){
+    $('.p2').hide();
+    setTimeout(function(){                           //intro text
+    $('.p2').fadeIn('slow');  
+    }, 17750);
+})
+
+$(document).ready(function() {
+    $("#bossintro").animate({top: "-=0"}, 1800);        //ship movement
+    $("#bossintro").animate({top: "-=500"}, 8250);
+    $("#bossintro").animate({left: "-=2500"}, 1000);
+    $("#p1intro").animate({top: "-=0"}, 10500);
+    $("#p1intro").animate({left: "+=6000"}, 5000);
+    $("#p2intro").animate({top: "-=0"}, 12000);
+    $("#p2intro").animate({left: "-=6000"}, 5000); 
+    $("#p3intro").animate({top: "-=0"}, 13750);         
+    $("#p3intro").animate({left: "+=6000"}, 5000);
+    $("#p4intro").animate({top: "-=0"}, 15000);
+    $("#p4intro").animate({left: "-=6000"}, 5000);
+    $("#player1").animate({top: "-=0"}, 2000);
+    $("#player1").animate({left: "+=500"}, 4000);
+    $("#player2").animate({top: "-=0"}, 2000);
+    $("#player2").animate({left: "+=600"}, 4000);
 });
+
+
+
+
+
+
+
+
 
 });
